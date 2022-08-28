943c514176f962171c45928519c429ca850d2f2d
Mohshin Ahmed <mohshina1@outlook.com>
2022-08-28 03:27:22 +0100

    Complete exercise 2

---
+++ exercises/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,25 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutes)
+    {
+        return 40 - minutes;
+    }
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return 2 * layers;
+    }
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutes)
+    {
+        return PreparationTimeInMinutes(layers) + minutes;
+    }
+}
