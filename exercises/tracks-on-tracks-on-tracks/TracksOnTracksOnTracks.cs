ed01196bc4658532b48272e6fa245d3f81d35b05
Mohshin Ahmed <mohshina1@outlook.com>
2022-09-14 17:31:05 +0100

    Complete exercise

---
+++ exercises/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class Languages
+{
+    private static List<string> languagesToLearn = new List<string>() {"C#", "Clojure", "Elm"};
+    private static string excitingLanguage = "C#";
+    public static List<string> NewList()
+    {
+        return new List<string>();
+    }
+
+    public static List<string> GetExistingLanguages()
+    {
+        return languagesToLearn;
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        return languages.Append(language).ToList();
+    }
+
+    public static int CountLanguages(List<string> languages)
+    {
+        return languages.Count;
+    }
+
+    public static bool HasLanguage(List<string> languages, string language)
+    {
+        return languages.Contains(language);
+    }
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        return languages.Reverse<string>().ToList();
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        if (languages.Count == 0)
+        {
+            return false;
+        }
+        else if (languages[0] == excitingLanguage)
+        {
+            return true;
+        }
+        else if (languages[1] == excitingLanguage && (languages.Count is >= 2 and <= 3))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        return languages.Where(x => x!= language).ToList();
+    }
+
+    public static bool IsUnique(List<string> languages)
+    {
+        return languages.Distinct().Count() == languages.Count;
+    }
+}
